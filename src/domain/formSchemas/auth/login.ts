14f036954d3949faa39f24a83ae9df0319f40a17
joaolima98 <joaodeazevedolimaneto@gmail.com>
2024-08-22 02:00:48 -0300

    Added name to yup

--- src/domain/formSchemas/auth/login.ts
+++ src/domain/formSchemas/auth/login.ts
@@ -4,11 +4,14 @@ export const loginFormSchema = yup.object({
   email: yup
   .string()
   .email('Forneça um e-mail válido!')
-  .required('Campo nao pode ficar vazio!'),
+  .required('Campo obrigatorio!'),
   password: yup
   .string()
   .min(8, 'A senha precisa ter no minimo 8 caracteres')
   .required('Campo obrigatorio!'),
+  name: yup
+  .string()
+  .required('Campo Obrigatório!')
 })
 
 export type LoginForm = yup.InferType<typeof loginFormSchema>;
