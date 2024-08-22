5d75be533f0ed1a651911632f876cb8c75aaeeae
Victor Cassiano <jose_victor@live.com>
2024-08-22 14:18:53 -0300

    fix: fixed icons on focus

--- src/app/(auth)/(tabs)/_layout.tsx
+++ src/app/(auth)/(tabs)/_layout.tsx
@@ -1,10 +1,10 @@
-import '@/styles/global.css'
-import { Tabs, Slot } from 'expo-router';
-import React from 'react';
+import "@/styles/global.css";
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { TabBarIcon } from '../../../components/navigation/TabBarIcon';
-import { Colors } from '../../../constants/Colors';
-import { useColorScheme } from '../../../hooks/useColorScheme';
+import { TabBarIcon } from "../../../components/navigation/TabBarIcon";
+import { Colors } from "../../../constants/Colors";
+import { useColorScheme } from "../../../hooks/useColorScheme";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -12,33 +12,43 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Inicio',
+          title: "Inicio",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <TabBarIcon
+              name={focused ? "home" : "home-outline"}
+              color={color}
+            />
           ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Carrinho',
+          title: "Carrinho",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'cart'} color={color} />
+            <TabBarIcon
+              name={focused ? "cart" : "cart-outline"}
+              color={color}
+            />
           ),
         }}
       />
-            <Tabs.Screen
+      <Tabs.Screen
         name="profile"
         options={{
-          title: 'Perfil',
+          title: "Perfil",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'person'} color={color} />
+            <TabBarIcon
+              name={focused ? "person" : "person-outline"}
+              color={color}
+            />
           ),
         }}
       />
