3b3e8e3369b04332306f4e415f77e213e8706deb
joaolima98 <joaodeazevedolimaneto@gmail.com>
2024-09-02 12:33:18 -0300

    Using Hooker for TableProvider

--- src/app/(public)/_layout.tsx
+++ src/app/(public)/_layout.tsx
@@ -2,15 +2,20 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { SQLiteProvider } from "expo-sqlite";
 import { initializeDatabase } from "@/database/initializeDatabase";
+import { TableProvider } from "@/hooks/useTable";
+
 
 export default function AppLayout() {
   StatusBar({ translucent: true });
 
   return (
     <SQLiteProvider databaseName="test.db" onInit={initializeDatabase}>
+      <TableProvider>
+
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="login/index" />
       </Stack>
+      </TableProvider>
     </SQLiteProvider>
   );
 }
