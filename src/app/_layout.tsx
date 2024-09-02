693bc868f0580cba47cbcd2c91aab6cf9b0a93ac
joaolima98 <joaodeazevedolimaneto@gmail.com>
2024-09-02 12:31:26 -0300

    Added Notification Statements

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,11 +2,16 @@ import "react-native-reanimated";
 import "@/styles/global.css";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Stack } from "expo-router";
 import { SessionProvider } from "@/contexts/auth";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { PaperProvider } from "react-native-paper";
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+import { Alert, Platform } from "react-native";
+
 
 SplashScreen.preventAutoHideAsync();
 
@@ -15,6 +20,63 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
+
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
+  });
+
+  async function registerForPushNotificationsAsync() {
+    if (Platform.OS === 'android') {
+      await Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        Alert.alert('Você não receberá avisos dos agendamentos, a menos que permita notificações.');
+        return;
+      }
+      const token = await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig?.extra?.eas.projectId,
+      });
+
+      console.log(token);
+    } else {
+      Alert.alert('É necessário usar um dispositivo físico para notificações push.');
+    }
+  }
+
+  useEffect(() => {  
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+
+    });
+
+    return () => {
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(notificationListener.current);
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   useEffect(() => {
     if (loaded) {
@@ -29,6 +91,7 @@ export default function RootLayout() {
   return (
     <PaperProvider>
       <SessionProvider>
+        
         <Stack
           screenOptions={{
             headerShown: false,
