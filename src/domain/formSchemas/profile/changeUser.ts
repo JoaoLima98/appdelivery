b62b4b52e7a9cdc5818ebc47ee99cfa0482a70d5
joaolima98 <joaodeazevedolimaneto@gmail.com>
2024-09-03 20:37:03 -0300

    Divided name and email for validation, before it was generic

--- src/domain/formSchemas/profile/changeUser.ts
+++ src/domain/formSchemas/profile/changeUser.ts
@@ -1,8 +1,14 @@
 import * as yup from 'yup'
 
 export const changeUserFormSchema = yup.object({
-  value: yup
+  name: yup
   .string()
+  .required('Campo obrigatorio!'),
+
+  email: yup
+  .string()
+  .email('Forneça um e-mail válido!')
   .required('Campo obrigatorio!'),
 })
 
+export type ChangeForm = yup.InferType<typeof changeUserFormSchema>;
